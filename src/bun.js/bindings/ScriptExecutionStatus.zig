3953585db4f2b0a65d3d6fd2a037f34ff4ff4f68
MT224244 <25514849+MT224244@users.noreply.github.com>
2025-04-27 05:24:55 +0900

    🔥 bun-lambda以外を削除

--- src/bun.js/bindings/ScriptExecutionStatus.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const bun = @import("bun");
-
-pub const ScriptExecutionStatus = enum(i32) {
-    running = 0,
-    suspended = 1,
-    stopped = 2,
-};
--- src/bun.js/bindings/ZigErrorType.zig
+++
@@ -1,6 +0,0 @@
-pub const ZigErrorType = extern struct {
-    code: ErrorCode,
-    ptr: ?*anyopaque,
-};
-
-const ErrorCode = @import("ErrorCode.zig").ErrorCode;

aea95345e85a97716bb20f3f0b92686874c0d39a
MT224244 <25514849+MT224244@users.noreply.github.com>
2025-06-17 02:36:09 +0900

    🚚

--- src/cli/patch_commit_command.zig
+++
@@ -1,8 +0,0 @@
-const Command = @import("../cli.zig").Command;
-const PackageManager = @import("../install/install.zig").PackageManager;
-
-pub const PatchCommitCommand = struct {
-    pub fn exec(ctx: Command.Context) !void {
-        try PackageManager.patchCommit(ctx);
-    }
-};
